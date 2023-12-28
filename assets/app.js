e6fd238a42981706a20009fefa638a64166e6258
shanawazaga <merchantzeshanimtiaz@gmail.com>
2023-12-28 14:03:09 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,29 @@
-// // NAVBAR
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.getElementById("navMenu");
+// NAVBAR
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.getElementById("navMenu");
 
-// hamburger.addEventListener("click",()=>{
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// })
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-// document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
-//   hamburger.classList.remove("active");
-//   navMenu.classList.remove("active");
-// }))
-// // NAVBAR
+document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+// NAVBAR
 
 
-// // HERO TITLE
-//  let words = ["Website","Application","Wordpress"]
-// function changeText(){
-//   i = 0;
-//   setInterval(function(){ $('#words').fadeOut(function(){
-//       $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
-//     }); }, 2000)
-// }
-// changeText();
-// // HERO TITLE
+// HERO TITLE
+ let words = ["Website","Application","Wordpress"]
+function changeText(){
+  i = 0;
+  setInterval(function(){ $('#words').fadeOut(function(){
+      $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
+    }); }, 2000)
+}
+changeText();
+// HERO TITLE
 
 // !FORM VALIDATION
 const usernameEl = document.querySelector('#username');
